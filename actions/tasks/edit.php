061f6a353a1bda27e5e1dace400a81392b6aab5a
Niccolò Cavallini <ncavallini@student.ethz.ch>
2024-11-20 09:19:30 +0100

    Edit tasks

---
+++ actions/tasks/edit.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . "/../../utils/init.php";
+if(!Auth::is_logged_in()) {
+    header("Location: ../../index.php?page=login");
+    die;
+}
+
+$connection = DBConnection::get_connection();
+
+$is_group_task = $_POST['group_id'] != 0;
+if($is_group_task) {
+    $sql = "UPDATE group_tasks SET group_id = ?, title = ?, due_date = ?, location = ?, description = ?, estimated_load = ?, category = ? WHERE task_id = ?";
+    $stmt = $connection->prepare($sql);
+    $stmt->execute([$_POST['group_id'], $_POST['title'], $_POST['due_date'], $_POST['location'], $_POST['description'], $_POST['estimated_load'], $_POST['category'], $_POST['task_id']]);
+    $location = "../../index.php?page=group&group_id=$group_id"; 
+}
+else {
+    $sql = "UPDATE tasks SET title = ?, due_date = ?, location = ?, description = ?, estimated_load = ?, category = ? WHERE task_id = ?";
+    $stmt = $connection->prepare($sql);
+    $stmt->execute([$_POST['title'], $_POST['due_date'], $_POST['location'], $_POST['description'], $_POST['estimated_load'], $_POST['category'], $_POST['task_id']]);
+    $location = "../../index.php?page=manage_personal";
+}
+
+header("Location: $location");
+
+?>
