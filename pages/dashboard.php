350e5c93f20f03a01aae622e99e21218bf59f4fa
Niccolò Cavallini <ncavallini@student.ethz.ch>
2024-11-21 14:43:36 +0100

    Updated dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -16,7 +16,8 @@
     echo "<div class='col'>";
     echo "<div class='card'>";
     echo "<div class='card-body'>";
-    echo "<p class='h5'></p>";
+    echo "<p class='h5'>{$group['name']}</p>";
+    echo "</div>";
     echo "</div>";
     echo "</div>";
   }
@@ -24,44 +25,7 @@
   ?>
 </div>
 
-<div class="row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="..." class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-    </div>
-  </div>
-</div>
+
 
 <div class="list-group">
   <a href="index.php?page=manage" class="text-center list-group-item list-group-item-action active"><i class="fa-solid fa-gear"></i> Manage</a>
