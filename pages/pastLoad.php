0e653bf05b40c29a4cefaed2b1ddaffa57df0347
Eren Homburg <ehomburg@student.ethz.ch>
2024-11-29 10:04:43 +0100

    change the title of the overlay

--- pages/pastLoad.php
+++ pages/pastLoad.php
@@ -106,7 +106,7 @@ $taskDetails = json_encode($groupedData); // Task details grouped by date
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="taskDetailsModalLabel">Task Details</h5>
+                    <h5 class="modal-title" id="taskDetailsModalLabel">Contributing Tasks</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
